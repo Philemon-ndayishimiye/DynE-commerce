2d88f1f97dc8a419948d62158a6e4398dc3d543c
Philemon-ndayishimiye <philemonndayi@gmail.com>
2025-05-13 15:26:17 +0200

    footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,13 +1,76 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
 
-    <div>
-      <div>
-        <h1>hello every one</h1>
-        <h3>hello pacento</h3>
-      </div>
+    <div className='bg-[#1F2B48] py-3'>
+      <div className='ml-10 py-7'> <h1 className='text-white font-semibold text-xl'>E-commerce</h1> </div>
+
+       <div className='flex'> 
+
+         <div className='bg-[#1C2846] ml-10 w-[21%] py-4 h-[370px]'>
+            <h1 className='text-white text-xl font-semibold py-2 ml-3 mb-7'>About</h1>
+
+            <div className='flex flex-col space-y-4'>
+                <Link className='text-white  px-3'>About</Link>
+                <Link className='text-white  px-3'>Submit on Issue</Link>
+                <Link className='text-white  px-3'>Github Repo</Link>
+                <Link className='text-white  px-3'>Slack</Link>
+
+            </div>
+         </div>
+
+          <div className='bg-[#1C2846] ml-10 w-[21%] py-4 h-[370px]'>
+            <h1 className='text-white text-xl font-semibold py-2 ml-3 mb-7'>GETTING STARTED</h1>
+
+            <div className='flex flex-col space-y-4'>
+                <Link className='text-white  px-3'>Introduction</Link>
+                <Link className='text-white  px-3'>Documentation</Link>
+                <Link className='text-white  px-3'>Usage</Link>
+                <Link className='text-white  px-3'>Globals</Link>
+                <Link className='text-white  px-3'>Elements</Link>
+                <Link className='text-white  px-3'>Collections</Link>
+                <Link className='text-white  px-3'>Themes</Link>
+
+            </div>
+         </div>
+
+          <div className='bg-[#1C2846] ml-10 w-[21%] py-4 h-[370px]'>
+            <h1 className='text-white text-xl font-semibold py-2 ml-3 mb-7'>Resources</h1>
+
+            <div className='flex flex-col space-y-4'>
+                <Link className='text-white  px-3'>API</Link>
+                <Link className='text-white  px-3'>FormValidation product</Link>
+                <Link className='text-white  px-3'>Viability</Link>
+                <Link className='text-white  px-3'>Accessibility</Link>
+                <Link className='text-white  px-3'>community</Link>
+                <Link className='text-white  px-3'>Design</Link>
+                <Link className='text-white  px-3'>Marketplace</Link>
+
+            </div>
+         </div>
+
+          <div className='bg-[#1C2846] ml-10 w-[21%] py-4 h-[370px]'>
+            <h1 className='text-white text-xl font-semibold py-2 ml-3 mb-7'>GETTING STARTED</h1>
+
+            <div className='flex flex-col space-y-4'>
+
+                <p className='text-white  px-3' > Follow us on social media</p>
+                <p className='text-white  px-3' > to find out the latest</p>
+                <p className='text-white  px-3' > updates on our progress</p>
+
+                <div className='py-3'>
+                    
+
+                </div>
+                
+                
+
+            </div>
+         </div>
+
+       </div>
     </div>
   )
 }
