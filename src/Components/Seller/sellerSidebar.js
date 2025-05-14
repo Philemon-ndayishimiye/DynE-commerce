f5814aa5f1314c3435b83f8ccbb387c0eea8e73f
Niyikora-123 <mignoneniyikoraa@gmail.com>
2025-05-14 11:04:04 -0700

    progress

--- src/Components/Seller/sellerSidebar.js
+++ src/Components/Seller/sellerSidebar.js
@@ -3,27 +3,30 @@ import{ HiHome, HiPlus, HiEye, HiChat, HiBell, HiLocationMarker, HiChartBar} fro
 import { FaUserAlt } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
+
 export default function SellerSidebar() {
 
   return(
+     
 
-     <div className= "bg-blue-300  w-[300px] ml-[20px] mt-[20px]">
-      <FaUserAlt/>
-      <div>
-        <h4>E SELLER</h4>
-        <h5>seller@company.com</h5>
+     <div className= "bg-blue-300 text-center w-[300px] ml-5 mt-5 p-5 rounded-lg">
+
+      <FaUserAlt className="text-4xl text-gray mx-auto"/>
+      <div className="text-black-500">
+        <div><h4 className= "text-2xl font-bold text-black mt-3">E SELLER</h4></div>
+        <div><p className="text-sm text-gray-600 mb-6">seller@company.com</p></div>
         
-    </div>
-       <div  className="space-y-4">
-         <div> <Link className="flex"> <HiHome  className="mr-2"/> Home</Link></div>
-         <div><Link className="flex"><HiPlus className="mr-2"/>Add Product</Link></div>
-         <div><Link className="flex"><HiEye className="mr-2"/>View Product</Link></div>
-         <div><Link className="flex"><HiEye className="mr-2"/>View Product</Link></div>
-         <div><Link className="flex"><HiChat className="mr-2"/>Messages</Link></div>
-         <div><Link className="flex"><HiBell className="mr-2"/>Notification</Link></div>
-         <div><Link className="flex"><HiLocationMarker className="mr-2"/>Location</Link></div>
-         <div><Link className="flex"><HiChartBar className="mr-2"/>Graph</Link></div>
+    </div >
+       <div  className="space-y-4 justify-center p-[20px]">
+         <div> <Link className="flex "> <HiHome  className="mr-2 text-lg"/> Home</Link></div>
+         <div><Link className="flex "><HiPlus className="mr-2 text-lg"/>Add Product</Link></div>
+         <div><Link className="flex"><HiEye className="mr-2 text-lg"/>View Product</Link></div>
+         <div><Link className="flex"><HiChat className="mr-2 text-lg"/>Messages</Link></div>
+         <div><Link className="flex"><HiBell className="mr-2 text-lg"/>Notification</Link></div>
+         <div><Link className="flex"><HiLocationMarker className="mr-2 text-lg"/>Location</Link></div>
+         <div><Link className="flex"><HiChartBar className="mr-2 text-lg"/>Graph</Link></div>
        </div>
-     </div>   
+     </div> 
+      
   )
 }
